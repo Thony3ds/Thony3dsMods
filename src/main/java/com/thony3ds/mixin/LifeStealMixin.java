b401b6a6f0cbb87a9756d2e56667714e775438a0
Thony3ds <hiphop.antho@gmail.com>
2025-06-11 14:08:22 +0200

    LifeSteal done

--- src/main/java/com/thony3ds/mixin/LifeStealMixin.java
+++ src/main/java/com/thony3ds/mixin/LifeStealMixin.java
@@ -1,5 +1,6 @@
 package com.thony3ds.mixin;
 
+import com.thony3ds.Thony3dsMods;
 import com.thony3ds.util.KeepInventory;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -15,10 +16,7 @@ public abstract class LifeStealMixin {
     @Inject(method="onDeath", at= @At("TAIL"))
     public void onPlayerDeath(DamageSource source, CallbackInfo ci){
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
-        if (player.isDead()) {
-            return;
-        }
-        if (!player.getWorld().getRegistryKey().toString().contains("thony3dsmods:eternity_trials") && player.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() > 10) {
+        if (!(player.getWorld().getRegistryKey().toString().contains("thony3dsmods:eternity_trials")) && player.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() > 10) {
             if (source.getAttacker() instanceof PlayerEntity attacker) {
                 System.out.println("Player: "+player.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() + "\nAttacker: "+ attacker.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue());
                 player.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(player.getMaxHealth() - 2);
@@ -26,15 +24,16 @@ public abstract class LifeStealMixin {
                 if (attacker.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() > 40) {
                     attacker.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(40);
                 }
-                if (player.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() < 10){
-                    player.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(10);
-                }
                 System.out.println("Player: "+player.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() + "\nAttacker: "+ attacker.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue());
             } else {
                 player.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(player.getMaxHealth() - 1);
             }
+            if (player.getAttributeInstance(EntityAttributes.MAX_HEALTH).getBaseValue() < 10){
+                player.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(10);
+            }
         }
     }
+
     @Inject(method="onDeath", at=@At("HEAD"))
     public void keepInventoryOnVoidDeath(DamageSource source, CallbackInfo ci){
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
